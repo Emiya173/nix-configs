d2ed99f607c981da0a172f843a02df89a8a1e438
Emiya173 <cno.101@qq.com>
2026-05-08 01:45:42 +0800

    feat(grub): 接入 Navi 主题 + 对齐 GFXMODE/saved-default/quiet

--- modules/boot.nix
+++ modules/boot.nix
@@ -15,11 +15,25 @@
         efiSupport = true;
         useOSProber = true;
         configurationLimit = 20;
+
+        # 记住上次选择的启动项 (= GRUB_DEFAULT=saved + GRUB_SAVEDEFAULT=true)
+        default = "saved";
+
+        # Navi 主题 (从 Arch /usr/share/grub/themes/Navi 拷过来)
+        theme = ../assets/grub-theme-navi;
+        splashImage = ../assets/grub-theme-navi/background.png;
+
+        # 与 Arch /etc/default/grub 的 GRUB_GFXMODE 一致 (2K 优先)
+        gfxmodeEfi = "2560x1440x32,1920x1080x32,1280x1024x32,auto";
+        gfxmodeBios = "2560x1440x32,1920x1080x32,1280x1024x32,auto";
+        gfxpayloadEfi = "keep";
       };
       timeout = 5;
     };
 
     supportedFilesystems = [ "btrfs" "ntfs" ];
+
+    kernelParams = [ "quiet" ];
 
     kernel.sysctl = {
       "vm.swappiness" = 10;
